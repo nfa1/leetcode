a5854ab0c4b6a50ef9115e8b47f9b0b626194e04
Nilo Alvarado <34761234+nfa1@users.noreply.github.com>
2024-03-13 05:38:48 -0400

    Time: 90 ms (85.59%), Space: 13.7 MB (78.25%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        result = 0
+        for num in nums:
+            result ^=num
+        return result
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
