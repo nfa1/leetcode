7c7ce321034a29376a7f36a83c4306fd72a17f05
Nilo Alvarado <34761234+nfa1@users.noreply.github.com>
2024-07-25 02:45:02 -0400

    Time: 429 ms (50.47%), Space: 27.5 MB (14.97%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,14 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        # initialize an empty set
-        seen_elements = set()
         
-        # iterate through the list
-        for num in nums: 
-            if num in seen_elements:
-                return True # Duplicate found
-            seen_elements.add(num) # add element to hash set
-        return False # No duplicates found
-    
+        seen = set()
+        
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+
+            
+
         
